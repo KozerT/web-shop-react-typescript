6c6e513db8e607248ae0223907503f94bac46e27
KozerT <kozer.vizastaff@gmail.com>
2023-12-01 16:51:34 +0100

    build: react vite project created

---
+++ src/components/Product.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Product = () => {
+  return (
+    <div className="border py-2 px-4 rounded flex flex-col items-center mb-2">
+      Product
+    </div>
+  );
+};
+
+export default Product;

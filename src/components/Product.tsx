b03cc380f14af2005756896528b57e3563660b84
KozerT <kozer.vizastaff@gmail.com>
2023-12-01 17:25:31 +0100

    feat: added a button to display details

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import { Iproduct } from "../models";
 
-const Product = () => {
+interface ProductProps {
+  product: Iproduct;
+}
+
+const Product = ({ product }: ProductProps) => {
+  const [details, setDetails] = useState(false);
+  const btnBgCLassName = details ? "bg-blue-300" : "bg-yellow-300";
+
+  const btnClasses = ["py-2 px-4 border", btnBgCLassName];
+
   return (
     <div className="border py-2 px-4 rounded flex flex-col items-center mb-2">
-      Product
+      <img src={product.image} alt={product.title} className=" w-24" />
+      <p>{product.title}</p>
+      <p className="font-bold">{product.price}</p>
+      <button
+        className={btnClasses.join(" ")}
+        onClick={() => setDetails((prev) => !prev)}
+      >
+        {details ? "Hide Details" : "Show Details"}
+      </button>
+
+      {details && (
+        <div>
+          <p>{product.description} </p>
+          <p>
+            Rate:
+            <span style={{ fontWeight: "bold" }}>
+              {product.rating.rate}
+            </span>{" "}
+          </p>
+        </div>
+      )}
     </div>
   );
 };

a05a70a5a7cc965ee3c0cf9068c02cca2a1b1caa
KozerT <kozer.vizastaff@gmail.com>
2023-12-04 23:30:35 +0100

    style(footer): implement basic styling for Footer

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Navigation = () => {
   return (
     <nav className="h-[48px] flex justify-between px-5 text-white items-center bg-black">
-      <span className="font-bold">Web shop</span>
+      <span className="font-bold">Webshop</span>
       <span>
         <Link to="/" className=" mr-2">
           Products
